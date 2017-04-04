ec421992b7bb8dc48fd146e39ec300877b704b5a
Victoria Leftwich <victorialeftwich@Victorias-MacBook-Pro.local>
2017-04-04 20:42:27 +0100

    restricted output to first 50 tweets

--- sentiments/analyzer.py
+++ sentiments/analyzer.py
@@ -6,7 +6,7 @@ class Analyzer():
     """Implements sentiment analysis."""
 
     def __init__(self, positives, negatives):
-        
+
         self.positiveList = []
         self.negativeList = []
         
@@ -16,34 +16,34 @@ class Analyzer():
             for line in positivesFile:
                 cleanedLine = line.strip()
                 if cleanedLine:
-                    
+
                     self.positiveList.append(cleanedLine)
-                    
-                
-                
+
+
+
         with open(negatives) as negativesFile:
             for _ in range(35):
                 next(negativesFile)
             for line in negativesFile:
                 cleanedLine = line.strip()
                 if cleanedLine:
-                    
+
                     self.negativeList.append(cleanedLine)
 
         positivesFile.close()
         negativesFile.close()
-        
-        
+
+
 
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
         #print("text= {}".format(text))
-        
+
         score = 0
-        
+
         textList = text.split()
-        
-        
+
+
         for i in textList:
             i = i.lower()
             for positiveWord in self.positiveList:
@@ -54,10 +54,6 @@ class Analyzer():
             for negativeWord in self.negativeList:
                 if i == negativeWord:
                     score -= 1
-        
-                    
-            
-        
-        # TODO
+
+
         return score
-        
